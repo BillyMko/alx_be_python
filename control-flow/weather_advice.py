4f338a6bd7681d65ec70299f264003b89e366a8d
BillyMko <mukobelieve@gmail.com>
2025-11-16 17:11:48 +0200

    weather conditions if statements

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,14 @@
+weather_condition = input("What's the weather like today? (sunny/rain/cold):. ")
+
+if weather_condition == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+
+elif weather_condition == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+
+elif weather_condition == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+
+else:
+    print("Sorry, l don't have recommendations for this weather.")
+    
