b077be7c7ba432ceec32a88d39c7717a00f67d0a
BillyMko <mukobelieve@gmail.com>
2025-11-30 20:31:52 +0200

    class that encapsulates banking operation

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,25 +1,16 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
-        self.__account_balance = initial_balance
+        self.account_balance = initial_balance
+
     def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            print(f"Deposited: ${amount:.2f}")
-        else:
-            print("Deposit amount must be positive.")
+        self.account_balance += amount
 
     def withdraw(self, amount):
-        if amount <= 0:
-            print("Withdrawal amount must be positive.")
-            return False
-        if amount <= self.__account_balance:
-            self.__account_balance -= amount
-            print(f"Withdrew: ${amount:.2f}")
+        if amount <= self.account_balance:
+            self.account_balance -= amount
             return True
-        else:
-            print("Insufficient funds.")
-            return False
+        return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.__account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance}")
 
