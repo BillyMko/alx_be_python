882e27703c26cdbdebfa647db36becd56dc9fd9c
BillyMko <mukobelieve@gmail.com>
2025-11-30 21:01:26 +0200

    OOP for a Library Management System

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,16 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+
